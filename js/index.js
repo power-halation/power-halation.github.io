481cea9c4b0869b7b220e515d76bb15ccac39a86
TSUYUSATO Kitsune <make.just.on@gmail.com>
2016-12-28 00:10:44 +0900

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+{
+  // initialize smooth-scroll
+  smoothScroll.init()
+
+  // set-up to click to enter
+  const background = document.getElementById('top')
+  const backgroundLogo = document.getElementById('top-logo')
+
+  const toMain = () => {
+    background.setAttribute('data-state', 'main')
+  }
+
+  backgroundLogo.addEventListener('mousedown', event => {
+    toMain()
+  })
+
+  // ...or, go to main by accessing url with hash
+  if (location.hash !== '') {
+    toMain()
+    smoothScroll.animateScroll(document.querySelector(location.hash))
+  }
+}
